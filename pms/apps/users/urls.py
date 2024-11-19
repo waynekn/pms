a132b41fe1bd80b4f8508ce50d4280644febd24f
Wayne Kinyanjui <waynekn24@gmail.com>
2024-11-19 22:33:02 +0300

    add url to view a users profile page

--- pms/apps/users/urls.py
+++ pms/apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from .views import HomeView
+from .views import HomeView, ProfilePageView
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('accounts/', include('allauth.urls')),
+    path('user/<str:username>/', ProfilePageView.as_view(), name='profile_page'),
 ]
