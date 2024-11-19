556cacf78233d3cc04a665b29ea0d1dff141618a
Wayne Kinyanjui <waynekn24@gmail.com>
2024-11-19 14:03:47 +0300

    register `OrganizationMembers` model with admin

--- pms/apps/organizations/admin.py
+++ pms/apps/organizations/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Organization
+from .models import Organization, OrganizationMembers
 
 # Register your models here.
 
 admin.site.register(Organization)
+admin.site.register(OrganizationMembers)
