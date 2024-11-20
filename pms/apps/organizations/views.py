70f8daf02600f6fa61dc6ed6a8ea97d03186c51d
Wayne Kinyanjui <waynekn24@gmail.com>
2024-11-20 13:49:25 +0300

    add `UserOrganizationListView`

--- pms/apps/organizations/views.py
+++ pms/apps/organizations/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Organization, OrganizationMembers
+from .serializers import OrganizationSerializer
 
 # Create your views here.
+
+
+class UserOrganizationListView(APIView):
+    """
+    Displays a list of organizations that the currently authenticated user
+    is a member of.
+    """
+    model = Organization
+
+    def get(self, request) -> Response:
+        # Get all OrganizationMembers entries where the user is a member
+        user_organizations = OrganizationMembers.objects.filter(
+            user=self.request.user)
+
+        # Get the related Organization objects for those memberships
+        organizations = Organization.objects.filter(
+            organization_id__in=user_organizations.values('organization_id')
+        )
+
+        serializer = OrganizationSerializer(organizations, many=True)
+        return Response(serializer.data)
