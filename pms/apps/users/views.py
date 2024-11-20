f1e5cffe728db68f7628c5ca23c8382fc41db2ce
Wayne Kinyanjui <waynekn24@gmail.com>
2024-11-20 13:48:43 +0300

    add profile page view

--- pms/apps/users/views.py
+++ pms/apps/users/views.py
@@ -1,3 +1,8 @@
+from typing import Any
+from django.http import HttpRequest
+from django.shortcuts import redirect
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 # Create your views here.
@@ -5,3 +10,20 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'users/home.html'
+
+
+class ProfilePageView(LoginRequiredMixin, TemplateView):
+    """
+    Returns a user's profile page. If the user is not authenticated or is trying to view a profile
+    that is not theirs, they will be redirected to the login page or the their own page.
+    """
+    template_name = 'users/profile_page.html'
+
+    def dispatch(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse | HttpResponseRedirect:
+        username = kwargs.get('username')
+
+        if request.user.username != username:
+            # Redirect to the user's own profile if they are trying to access someone else's profile
+            return redirect('profile_page', username=request.user.username)
+
+        return super().dispatch(request, *args, **kwargs)
