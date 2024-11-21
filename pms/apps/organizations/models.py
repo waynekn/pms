78a692301a070e37309074f93ba2c68e2ed40f5c
Wayne Kinyanjui <waynekn24@gmail.com>
2024-11-21 12:33:35 +0300

    add `organization_name_slug` and migration

--- pms/apps/organizations/models.py
+++ pms/apps/organizations/models.py
@@ -13,6 +13,8 @@ class Organization(models.Model):
         primary_key=True, default=uuid.uuid4, editable=False, verbose_name='Organization ID')
     organization_name = models.CharField(
         max_length=50, unique=True, verbose_name='Organization name')
+    organization_name_slug = models.SlugField(
+        max_length=100, verbose_name="Organization Slug", null=True)
     admin = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='organizations', verbose_name='Admin')
     organization_password = models.CharField(
