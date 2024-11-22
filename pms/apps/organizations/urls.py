09b328ab83278a7893ea6ff8710931318c7e55d0
Wayne Kinyanjui <waynekn24@gmail.com>
2024-11-23 00:11:24 +0300

    add link to create an organization

--- pms/apps/organizations/urls.py
+++ pms/apps/organizations/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import UserOrganizationListView
+from .views import UserOrganizationListView, OrganizationCreateView
 
 urlpatterns = [
     path('', UserOrganizationListView.as_view(), name='user_organizations'),
+    path('create/', OrganizationCreateView.as_view(), name='create_organization'),
 ]
