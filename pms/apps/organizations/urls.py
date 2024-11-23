e2ae3281d20ce4003e6b2307a951e75444d043f4
Wayne Kinyanjui <waynekn24@gmail.com>
2024-11-23 17:19:32 +0300

    add url to enable searching for organizations

--- pms/apps/organizations/urls.py
+++ pms/apps/organizations/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import UserOrganizationListView, OrganizationCreateView
+from .views import (UserOrganizationListView,
+                    OrganizationCreateView, OrganizationSearchView)
 
 urlpatterns = [
     path('', UserOrganizationListView.as_view(), name='user_organizations'),
     path('create/', OrganizationCreateView.as_view(), name='create_organization'),
+    path('search/', OrganizationSearchView.as_view(), name='organization_search'),
 ]
