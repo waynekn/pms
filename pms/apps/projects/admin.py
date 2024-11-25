a1c7b54f482c451750729b9574c0feef54cf3fbd
Wayne Kinyanjui <waynekn24@gmail.com>
2024-11-25 16:57:42 +0300

    register `Template` model with django admin

--- pms/apps/projects/admin.py
+++ pms/apps/projects/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Industry
+from .models import (Industry, Template)
 
 # Register your models here.
 
 admin.site.register(Industry)
+
+admin.site.register(Template)
