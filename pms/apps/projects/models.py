1162ac3495a57d5ecd1f50a15aa859062d9407dd
Wayne Kinyanjui <waynekn24@gmail.com>
2024-11-25 16:56:35 +0300

    add `Template` model and migration

--- pms/apps/projects/models.py
+++ pms/apps/projects/models.py
@@ -24,3 +24,42 @@ class Industry(models.Model):
 
     def __str__(self) -> str:
         return self.industry_name
+
+
+def get_default_industry():
+    """
+    Retrieves the default industry to be used when a template references a deleted industry.
+
+    Returns:
+        UUID: The unique identifier (industry_id) of the default industry, 
+            which is the "Other" industry.
+    """
+    industry = Industry.objects.get_or_create(industry_name="Other")
+    return industry.pk
+
+
+class Template(models.Model):
+    """
+    Represents a reusable project template that is categorized by industry.
+
+    The `Template` model stores project templates that users can apply to their own 
+    projects. Each template is linked to a specific `Industry`, allowing for better 
+    organization and searchability.
+
+    Attributes:
+        industry_id (ForeignKey): A reference to the `Industry` that the template 
+                                   belongs to. If the industry is deleted, it is 
+                                   set to the default industry using `get_default_industry`.
+        template_id (UUIDField): A unique identifier for the template, automatically 
+                                 generated using UUID.
+        template_name (CharField): The name of the template, which should be unique 
+                                   within the database.
+    """
+    industry_id = models.ForeignKey(Industry, on_delete=models.SET_DEFAULT,
+                                    default=get_default_industry)
+    template_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4)
+    template_name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return self.template_name
