6ef466e3d2f34fe609325b90c453379b623d762a
Wayne Kinyanjui <waynekn24@gmail.com>
2024-11-27 17:42:28 +0300

    register `TemplatePhase` model with django admin

--- pms/apps/projects/admin.py
+++ pms/apps/projects/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import (Industry, Template)
+from .models import (Industry, Template, TemplatePhase)
 
 # Register your models here.
 
 admin.site.register(Industry)
 
 admin.site.register(Template)
+
+admin.site.register(TemplatePhase)
