7975dd5657c277c35b7656c05e042580642d3a39
Wayne Kinyanjui <waynekn24@gmail.com>
2024-11-29 02:45:27 +0300

    register renamed `OrganizationMember` model

--- pms/apps/organizations/admin.py
+++ pms/apps/organizations/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Organization, OrganizationMembers
+from .models import Organization, OrganizationMember
 
 # Register your models here.
 
 admin.site.register(Organization)
-admin.site.register(OrganizationMembers)
+admin.site.register(OrganizationMember)
