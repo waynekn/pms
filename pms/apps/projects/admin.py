418723ce103f5d9082e4ff0b0b1edbb52cd2bb12
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-03 16:04:55 +0300

    register `Project` model with admin

--- pms/apps/projects/admin.py
+++ pms/apps/projects/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from .models import (Industry, Template, TemplatePhase)
+from .models import (Industry, Template, TemplatePhase, Project)
 
 # Register your models here.
 
 admin.site.register(Industry)
-
 admin.site.register(Template)
-
 admin.site.register(TemplatePhase)
+admin.site.register(Project)
