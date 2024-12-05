0c015b776a223a7bbc6ee3c472634317f8c0e22d
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-05 19:53:57 +0300

    add url to view organization detail

--- pms/apps/organizations/urls.py
+++ pms/apps/organizations/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
 from .views import (UserOrganizationListView,
-                    OrganizationCreateView, OrganizationSearchView)
+                    OrganizationCreateView, OrganizationSearchView, OrganizationDetailView)
 
 urlpatterns = [
     path('', UserOrganizationListView.as_view(), name='user_organizations'),
     path('create/', OrganizationCreateView.as_view(), name='create_organization'),
     path('search/', OrganizationSearchView.as_view(), name='organization_search'),
+    path('<slug:organization_name_slug>/',
+         OrganizationDetailView.as_view(), name='organization_detail'),
 ]
