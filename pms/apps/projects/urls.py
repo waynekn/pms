045d5ac43c7946deadc1b5682bcd7ac233b2e26e
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-06 11:35:46 +0300

    add url to create a project

--- pms/apps/projects/urls.py
+++ pms/apps/projects/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TemplateCreateView
+from .views import TemplateCreateView, ProjectCreateView
 
 urlpatterns = [
     path('template/create/', TemplateCreateView.as_view(), name='create_template'),
+    path('<slug:organization_name_slug>/project/create/',
+         ProjectCreateView.as_view(), name='create_project'),
 ]
