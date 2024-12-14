c8642e5b4a22b59508bcf020dac6ab989071e34c
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-14 16:25:50 +0300

    set `withCredentials` to true

--- pms/frontend/src/api.ts
+++ pms/frontend/src/api.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 // Create an Axios instance with pre-configured settings
 const api = axios.create({
   baseURL: "http://localhost:8000/",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
