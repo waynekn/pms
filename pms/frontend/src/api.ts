da159861a065a6d7e49c3c94c9e43c704f847aba
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-14 16:27:30 +0300

    add interceptor to include csrf token for post requests

--- pms/frontend/src/api.ts
+++ pms/frontend/src/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getCSRFToken } from "./utils/CSRFtoken";
 
 // Create an Axios instance with pre-configured settings
 const api = axios.create({
@@ -8,5 +9,19 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+//Add CSRF token for POST requests
+api.interceptors.request.use(
+  (config) => {
+    if (config.method === "post") {
+      config.headers["X-CSRFToken"] = getCSRFToken();
+    }
+    return config;
+  },
+  function (error) {
+    // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
+    return Promise.reject(error);
+  }
+);
 
 export default api;
