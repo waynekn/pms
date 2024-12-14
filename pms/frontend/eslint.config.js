072d56ede9ab309d945890542508f763425da5ef
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-14 19:10:49 +0300

    disable promise misuse check for void-returning functions

--- pms/frontend/eslint.config.js
+++ pms/frontend/eslint.config.js
@@ -30,6 +30,14 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      "@typescript-eslint/no-misused-promises": [
+        2,
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
     },
   }
 );
