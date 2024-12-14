28736806b35b7f3408004e31e4a7e99370738fed
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-14 17:51:15 +0300

    add url to get csrf token during development

--- pms/apps/users/urls.py
+++ pms/apps/users/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-
-from .views import HomeView, ProfilePageView
+from .views import (HomeView, ProfilePageView, csrf_token_view)
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('accounts/', include('allauth.urls')),
     path('user/<str:username>/', ProfilePageView.as_view(), name='profile_page'),
+    path('csrftoken/', csrf_token_view, name='get_csrftoken'),
 ]
