b6ce73e4f534359b47d6c1667f07c446e21a6b46
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-16 00:33:44 +0300

    configure Vite to build assets in Django's static directory

--- pms/frontend/vite.config.ts
+++ pms/frontend/vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    postcss: "./postcss.config.js",
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve("./src/main.tsx"),
+      },
+      output: {
+        dir: "../static/react/",
+        entryFileNames: "[name].js",
+      },
+    },
+  },
+});
