1ebfae2656eafbf5cde109f399c1c11a68088b13
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-16 12:43:56 +0300

    update URL pattern to use CSRFtokenView CBV

--- pms/apps/users/urls.py
+++ pms/apps/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from .views import (HomeView, ProfilePageView, csrf_token_view)
+from .views import (HomeView, ProfilePageView,
+                    CSRFtokenView,)
 
 
 urlpatterns = [
@@ -7,5 +8,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('user/<str:username>/', ProfilePageView.as_view(), name='profile_page'),
-    path('csrftoken/', csrf_token_view, name='get_csrftoken'),
+    path('csrftoken/', CSRFtokenView.as_view(), name='get_csrftoken'),
 ]
