ce456aece403b3acb1de0f436780e403e075fc27
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-16 12:56:28 +0300

    add docstring for UserDetailsSerializer

--- pms/apps/users/serializers.py
+++ pms/apps/users/serializers.py
@@ -3,6 +3,12 @@ from .models import User
 
 
 class UserDetailsSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for `REST_AUTH`. 
+
+    The fields defined here are what will be returned when
+    `dj-rest-auth` retreives a `User` from the database.
+    """
     class Meta:
         model = User
         fields = (
