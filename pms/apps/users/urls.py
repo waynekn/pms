d897a052f62cbdf5fcdf69f35c0d87de1112252b
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-17 13:11:20 +0300

    change position of allauth urls

--- pms/apps/users/urls.py
+++ pms/apps/users/urls.py
@@ -6,10 +6,10 @@ from .views import (HomeView, ProfilePageView,
 urlpatterns = [
     path('', FrontendView.as_view(), name='frontend'),
     path('', HomeView.as_view(), name='home'),
-    path('accounts/', include('allauth.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/google/',
          GoogleLogin.as_view(), name='google_login'),
     path('user/<str:username>/', ProfilePageView.as_view(), name='profile_page'),
     path('csrftoken/', CSRFtokenView.as_view(), name='get_csrftoken'),
+    path('accounts/', include('allauth.urls')),
 ]
