9ac98352dbc5f8fa30a01a725368b03f05767741
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-17 13:10:57 +0300

    add path for google auth

--- pms/apps/users/urls.py
+++ pms/apps/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .views import (HomeView, ProfilePageView,
-                    CSRFtokenView, FrontendView)
+                    CSRFtokenView, FrontendView, GoogleLogin,)
 
 
 urlpatterns = [
@@ -8,6 +8,8 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('accounts/', include('allauth.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/google/',
+         GoogleLogin.as_view(), name='google_login'),
     path('user/<str:username>/', ProfilePageView.as_view(), name='profile_page'),
     path('csrftoken/', CSRFtokenView.as_view(), name='get_csrftoken'),
 ]
