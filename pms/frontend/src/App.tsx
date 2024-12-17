782c42d56b89ba10249b3ee7ec8fcf96c0743ab7
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-17 11:05:39 +0300

    replace `process.env.NODE_ENV` with `import.meta.env.MODE`

--- pms/frontend/src/App.tsx
+++ pms/frontend/src/App.tsx
@@ -12,11 +12,11 @@ function App() {
      * This effect is used to fetch a CSRF token during the development environment.
      * It ensures that the CSRF token is retrieved before making any POST requests
      * to the API. This call will only be made when the app is running in
-     * development mode (`process.env.NODE_ENV === 'development'`).
+     * development mode (`import.meta.env.MODE === 'development'`).
      */
     const initCSRFToken = async () => {
       try {
-        if (process.env.NODE_ENV === "development") {
+        if (import.meta.env.MODE === "development") {
           await fetchCSRFtoken();
         }
         setIsLoading(false);
