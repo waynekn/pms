b93959cd222025997a17ada7c0ce05e675cf57a7
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-18 18:55:43 +0300

    add path for dj-rest-auth registration urls

--- pms/apps/users/urls.py
+++ pms/apps/users/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', FrontendView.as_view(), name='frontend'),
     path('', HomeView.as_view(), name='home'),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('dj-rest-auth/google/',
          GoogleLogin.as_view(), name='google_login'),
     path('user/<str:username>/', ProfilePageView.as_view(), name='profile_page'),
