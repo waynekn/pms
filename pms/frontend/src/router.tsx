63b2ea6b77417a06bd5698cf0d8c531c179dcf7f
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-18 17:26:28 +0300

    add `signup` route

--- pms/frontend/src/router.tsx
+++ pms/frontend/src/router.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router";
 
 import HomePage from "./pages/home-page";
 import LogInForm from "./components/login.component";
+import SignUpForm from "./components/signup.component";
 import GoogleCallback from "./components/google-callback.component";
 
 const Router = () => {
@@ -9,6 +10,7 @@ const Router = () => {
     <Routes>
       <Route path="/" element={<HomePage />}>
         <Route path="login/" element={<LogInForm />} />
+        <Route path="signup/" element={<SignUpForm />} />
       </Route>
       <Route
         path="/accounts/google/login/callback/"
