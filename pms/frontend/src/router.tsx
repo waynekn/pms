8aa0164beae381a608918653571fa4afa62f66da
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-18 13:45:58 +0300

    add `login` route as  a subroute to home route

--- pms/frontend/src/router.tsx
+++ pms/frontend/src/router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router";
 
 import HomePage from "./pages/home-page";
+import LogInForm from "./components/login.component";
 import GoogleCallback from "./components/google-callback.component";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<HomePage />}>
+        <Route path="login/" element={<LogInForm />} />
+      </Route>
       <Route
         path="/accounts/google/login/callback/"
         element={<GoogleCallback />}
