6046b75f199b0eb8b0cdbd7c483b7173bbaa4714
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-20 01:24:10 +0300

    wrap `App` in `PersistGate`

--- pms/frontend/src/main.tsx
+++ pms/frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./store/store.ts";
 import App from "./App.tsx";
 import store from "./store/store.ts";
 import "./output.css";
@@ -9,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
