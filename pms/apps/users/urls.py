eb339c087eee21a1ff79fc79f7a41b497110fd83
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-20 00:37:20 +0300

    add path to refresh access token

--- pms/apps/users/urls.py
+++ pms/apps/users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (ProfilePageView,
                     CSRFtokenView, GoogleLogin,)
 
@@ -8,6 +9,8 @@ urlpatterns = [
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('dj-rest-auth/google/',
          GoogleLogin.as_view(), name='google_login'),
+    path('dj-rest-auth/token/refresh/',
+         TokenRefreshView.as_view(), name="token_refresh"),
     path('user/<str:username>/', ProfilePageView.as_view(), name='profile_page'),
     path('csrftoken/', CSRFtokenView.as_view(), name='get_csrftoken'),
     path('accounts/', include('allauth.urls')),
