5eb93eff895e64bd1061873766238cd11050ae8b
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-20 00:24:48 +0300

    add token refresh interceptor to handle unauthorized errors

--- pms/frontend/src/api.ts
+++ pms/frontend/src/api.ts
@@ -1,5 +1,9 @@
-import axios from "axios";
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 import { getCSRFToken } from "./utils/CSRFtoken";
+
+type TokenRefreshResponse = {
+  access: string;
+};
 
 // Create an Axios instance with pre-configured settings
 const api = axios.create({
@@ -9,6 +13,58 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// Flag to prevent infinite loops
+let madeAttemptToGetAccessToken = false;
+
+/**
+ * Interceptor for unauthorized errors.
+ */
+api.interceptors.response.use(
+  (response: AxiosResponse) => response,
+  async (error: AxiosError) => {
+    const originalRequest = error.config as AxiosRequestConfig;
+
+    if (error.response?.status === 401) {
+      if (!madeAttemptToGetAccessToken) {
+        madeAttemptToGetAccessToken = true;
+
+        try {
+          const refreshResponse = await api.post<TokenRefreshResponse>(
+            "dj-rest-auth/token/refresh/"
+          );
+          const newAccessToken = refreshResponse.data.access;
+
+          // Reset the flag
+          madeAttemptToGetAccessToken = false;
+
+          // Update the Authorization header with the new token
+          originalRequest.headers = {
+            ...originalRequest.headers,
+            Authorization: `Bearer ${newAccessToken}`,
+          };
+
+          // Retry the original request with the new token
+          return api(originalRequest);
+        } catch (refreshError) {
+          // Redirect the user to the login page if unauthorized error persists.
+          window.location.href = "/login";
+          return Promise.reject(
+            refreshError instanceof Error
+              ? refreshError
+              : new Error(String(refreshError))
+          );
+        }
+      } else {
+        // Reset flag.
+        madeAttemptToGetAccessToken = false;
+        window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 //Add CSRF token for POST requests
 api.interceptors.request.use(
