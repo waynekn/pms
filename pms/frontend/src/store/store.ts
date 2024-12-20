8f054d175ee776724696d560fc1fd2b138dc5bce
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-20 15:47:17 +0300

    implement redux-persist for state persistence

--- pms/frontend/src/store/store.ts
+++ pms/frontend/src/store/store.ts
@@ -1,15 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["user"],
+};
+
+const persitedReducer = persistReducer(persistConfig, rootReducer);
 
 const isDev = import.meta.env.MODE === "development";
 
 const store = configureStore({
-  reducer: rootReducer,
-
+  reducer: persitedReducer,
   middleware: (getDefaultMiddleware) =>
     isDev ? getDefaultMiddleware().concat(logger) : getDefaultMiddleware(),
 });
+
+export const persistor = persistStore(store);
 
 export type StoreDispatch = typeof store.dispatch;
 
