57d0f666f85aacc9f2a6c432b4545f6cb8e76bd6
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-23 23:01:57 +0300

    add path to get industries

--- pms/apps/projects/urls.py
+++ pms/apps/projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TemplateCreateView, ProjectCreateView
+from .views import TemplateCreateView, ProjectCreateView, IndustryListView
 
 urlpatterns = [
+    path('industry/list/', IndustryListView.as_view(), name='industry_list'),
     path('template/create/', TemplateCreateView.as_view(), name='create_template'),
     path('<slug:organization_name_slug>/project/create/',
          ProjectCreateView.as_view(), name='create_project'),
