0ea72d515282fe0aaeb18ff0ad27a436d47a4856
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-25 17:33:47 +0300

    create `OrganizationAuthView` to handle the authorization process for a user to join an organization as an `OrganizationMember`.

--- pms/apps/organizations/views.py
+++ pms/apps/organizations/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import check_password
 from rest_framework import status
 from rest_framework import generics
 from rest_framework.response import Response
@@ -122,3 +123,40 @@ class OrganizationDetailView(APIView):
 
         return Response(
             organization_detail, status=status.HTTP_200_OK)
+
+
+class OrganizationAuthView(APIView):
+    """
+    Handles the authorization process for a user to join an organization as an `OrganizationMember`.
+
+    This view expects the following data in the request body:
+        - `organizationName`: The name of the organization the user wants to join.
+        - `password`: The password associated with the organization.
+
+    Responses:
+        - 201 Created: If the user is successfully added as an `OrganizationMember`.
+        - 400 Bad Request: If the credentials are invalid (either the organization doesn't exist, 
+          the password is incorrect, or the password is missing).
+    """
+
+    def post(self, request, *args, **kwargs):
+        organization_name = request.data.get('organizationName')
+        password = request.data.get('password')
+
+        try:
+            organization = Organization.objects.get(
+                organization_name=organization_name)
+        except Organization.DoesNotExist:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
+        password = password.strip() if password else None
+
+        if not password:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
+        if check_password(password, organization.organization_password):
+            OrganizationMember.objects.create(
+                organization=organization, user=self.request.user)
+            return Response(status=status.HTTP_201_CREATED)
+
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
