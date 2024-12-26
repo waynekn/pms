2467576311984019e2b3052b441927125d94d9d2
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-27 00:17:26 +0300

    add path to enable searching for templates

--- pms/apps/projects/urls.py
+++ pms/apps/projects/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TemplateCreateView, ProjectCreateView, IndustryListView
+from .views import TemplateCreateView, ProjectCreateView, IndustryListView, TemplateSearchView
 
 urlpatterns = [
     path('industry/list/', IndustryListView.as_view(), name='industry_list'),
     path('template/create/', TemplateCreateView.as_view(), name='create_template'),
+    path('template/search/', TemplateSearchView.as_view(), name='template_search'),
     path('<slug:organization_name_slug>/project/create/',
          ProjectCreateView.as_view(), name='create_project'),
 ]
