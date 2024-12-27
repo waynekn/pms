53ca94dc957c61a10a365ad3d2b4c53c01411136
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-27 13:04:05 +0300

    improve `ProjectSerializer` functionality to handle validation and creation of a project

--- pms/apps/projects/serializers.py
+++ pms/apps/projects/serializers.py
@@ -1,5 +1,8 @@
+from datetime import datetime, timezone
 from django.db import transaction
 from rest_framework import serializers
+from rest_framework.request import Request
+from .utils import slugify_project_name
 from . import models
 
 
@@ -102,4 +105,73 @@ class ProjectSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = models.Project
-        fields = '__all__'
+        fields = ['organization', 'template',
+                  'project_name', 'description', 'deadline']
+
+    def validate_description(self, description: str) -> str:
+        description = description.strip() if description else ''
+
+        if len(description) > 500:
+            raise serializers.ValidationError(
+                "Description is too long. Maximum length is 500 characters.")
+        return description
+
+    def validate_project_name(self, project_name):
+        project_name = project_name.strip() if project_name else None
+
+        if not project_name:
+            raise serializers.ValidationError(
+                "The project's name is required.")
+
+        if len(project_name) < 5:
+            raise serializers.ValidationError(
+                "A project's name must be at least 5 characters long.")
+
+        return project_name
+
+    def validate_deadline(self, deadline: str) -> str:
+        if deadline < datetime.now(timezone.utc).date():
+            raise serializers.ValidationError(
+                "Deadline cannot be in the past.")
+
+        return deadline
+
+    def validate(self, attrs):
+        organization = attrs.get('organization')
+        project_name = attrs.get('project_name')
+
+        if organization.projects.filter(project_name__iexact=project_name).exists():
+            raise serializers.ValidationError({
+                'project_name': 'A project with this name already exists in the organization.'
+            })
+
+        return super().validate(attrs)
+
+    @transaction.atomic
+    def create(self, validated_data):
+        request: Request = self.context.get('request')
+        template = validated_data.get('template')
+        deadline = validated_data.get('deadline')
+        organization = validated_data.get('organization')
+        project_name = validated_data.get('project_name')
+        description = validated_data.get('description')
+        project_name_slug = slugify_project_name(project_name)
+
+        if template:
+            project = models.Project.objects.create(organization=organization, template=template,
+                                                    project_name=project_name, project_name_slug=project_name_slug,
+                                                    description=description, deadline=deadline)
+        else:
+            project = models.Project.objects.create(organization=organization,
+                                                    project_name=project_name, project_name_slug=project_name_slug,
+                                                    description=description, deadline=deadline)
+
+        models.ProjectMember.objects.create(
+            member=request.user, project=project)
+
+        if template:
+            template_phases = [models.ProjectPhase(
+                project=project, template_phase=phase) for phase in template.phases.all()]
+            models.ProjectPhase.objects.bulk_create(template_phases)
+
+        return project
