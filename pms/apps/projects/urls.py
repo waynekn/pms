d30c470bf0a4f9781f1c8d505362123e46cf6755
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-27 16:55:23 +0300

    change path to `ProjectCreateView`

--- pms/apps/projects/urls.py
+++ pms/apps/projects/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('industry/list/', IndustryListView.as_view(), name='industry_list'),
     path('template/create/', TemplateCreateView.as_view(), name='create_template'),
     path('template/search/', TemplateSearchView.as_view(), name='template_search'),
-    path('<slug:organization_name_slug>/project/create/',
+    path('project/create/',
          ProjectCreateView.as_view(), name='create_project'),
 ]
