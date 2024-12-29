27f8e993e95c55d22f7cb3eb54ad2193f43dac46
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-30 02:25:02 +0300

    add tests for organization search functionality

--- pms/apps/organizations/tests/test_views.py
+++ pms/apps/organizations/tests/test_views.py
@@ -191,3 +191,60 @@ class TestOrganizationDetailRetreival(APITestCase):
         response = self.client.post(url, query, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class OrganizationSearchTest(APITestCase):
+    """
+    Tests for searching for organizations.
+    """
+
+    def setUp(self):
+        """
+        Set up an organization and user
+        """
+        self.user = User.objects.create_user(
+            username='testuser', email='testmail@test.com', password='securepassword123'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        url = reverse('create_organization')
+
+        data = {
+            'organization_name': 'Test org',
+            'description': 'organization description',
+            'organization_password': 'securepassword123',
+            'password2': 'securepassword123',
+        }
+
+        self.client.post(url, data, format='json')
+
+    def test_users_can_search_for_organizations(self):
+        """
+        Tests that a user can search for organizations.
+
+        This test ensures that:
+         - The response status code is HTTP 200 OK.
+         - The response data is a dictionary.
+        """
+        url = reverse('organization_search')
+        query = {
+            'organization_name_query': "T"
+        }
+
+        response = self.client.post(url, query, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, ReturnList)
+
+    def test_invalid_request_to_search_organizations(self):
+        """
+        Test that a bad request to search for organizations results in a 400
+        bad request error.
+        """
+        url = reverse('organization_search')
+        query = {
+            'name': "T"
+        }
+
+        response = self.client.post(url, query, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
