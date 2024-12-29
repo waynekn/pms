6db747757d572ac15138e9c5b95ac6ce0309adb4
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-29 14:14:30 +0300

    add test to verify that an organizations password is changed correctly

--- pms/apps/organizations/tests/test_models.py
+++ pms/apps/organizations/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.utils.text import slugify
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 from apps.organizations import models
 
 
@@ -32,3 +32,27 @@ class OrganizationModelTest(TestCase):
                          organization_name_slug)
         self.assertEqual(organization.organization_password,
                          organization_password)
+
+    def test_changing_organization_password(self):
+        """
+        Tests that an organization's password is updated correctly.
+
+        This test ensures that:
+         - The new password is updated correctly as the organization's password.
+        """
+        organization_name = 'Test org'
+        organization_name_slug = slugify(organization_name)
+        organization_password = make_password('securepassword123')
+
+        organization = models.Organization.objects.create(
+            organization_name=organization_name, organization_name_slug=organization_name_slug,
+            organization_password=organization_password)
+
+        new_password = 'newsecurepassword123'
+
+        organization.set_organization_password(new_password)
+
+        organization.refresh_from_db()
+
+        self.assertTrue(check_password(
+            new_password, organization.organization_password))
