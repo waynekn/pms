ad59ff5660a58534b608b031f7a7a3a94b49b294
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-30 14:40:06 +0300

    add tests for authenticating to join an organization

--- pms/apps/organizations/tests/test_views.py
+++ pms/apps/organizations/tests/test_views.py
@@ -248,3 +248,92 @@ class OrganizationSearchTest(APITestCase):
 
         response = self.client.post(url, query, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class OrganizationAuthenticationTests(APITestCase):
+    """
+    Tests the authentication feature of organization to become an organization
+    member.
+    """
+
+    def setUp(self):
+        """
+        Set up an organization, an organization member(creator of the organization)
+        and a user who is not a member of the organization.
+        """
+        self.user = User.objects.create_user(
+            username='orgmember', email='member@test.com', password='securepassword123'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+        url = reverse('create_organization')
+
+        data = {
+            'organization_name': 'Test org',
+            'description': 'organization description',
+            'organization_password': 'securepassword123',
+            'password2': 'securepassword123',
+        }
+        # create an organization.
+        self.client.post(url, data, format='json')
+
+    def test_non_members_can_authenticate_to_become_organization_members(self):
+        """
+        Test that a user who is not a member of an organization can authenticate
+        to join an organization.
+        """
+        url = reverse('organization_auth')
+
+        non_member = User.objects.create_user(
+            username='nonorgmember', email='nonmember@test.com', password='securepassword123'
+        )
+
+        client = APIClient()
+        client.force_authenticate(user=non_member)
+
+        credentials = {'password': 'securepassword123',
+                       'organizationName': 'Test org'}
+
+        response = client.post(url, credentials, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_invalid_auth_credentials_fail(self):
+        """
+        Tests that requests with wrong credentials and payload fails.
+
+        This test ensures that:
+         - A request with a wrong passwrod fails.
+         - A request with wrong payload fails.
+         - A request with wrong organization name fails.
+        """
+        url = reverse('organization_auth')
+        non_member = User.objects.create_user(
+            username='nonorgmember', email='nonmember@test.com', password='securepassword123'
+        )
+
+        wrong_password = {
+            'password': 'wrongpassword123',
+            'organizationName': 'Test org'
+        }
+
+        client = APIClient()
+        client.force_authenticate(user=non_member)
+        response = client.post(url, wrong_password, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        wrong_payload = {
+            'password': 'securepassword123',
+            'organization': 'Test org'
+        }
+
+        response = client.post(url, wrong_payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        wrong_org_name = {
+            'password': 'securepassword123',
+            'organizationName': 'wrong org'
+        }
+
+        response = client.post(url, wrong_org_name, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
