68a31c2911206b23ac304287cf80a0ec2b2b20f3
Wayne Kinyanjui <waynekn24@gmail.com>
2024-12-31 13:48:08 +0300

    create `Task` model

--- pms/apps/tasks/models.py
+++ pms/apps/tasks/models.py
@@ -17,3 +17,35 @@ class TaskCollection(models.Model):
     collection_name = models.CharField(
         verbose_name='Task name', db_comment='Collection name unique in the phase of the project', max_length=100)
     project_phase = models.ForeignKey(ProjectPhase, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    """
+    Represents a task within a task collection.
+    """
+
+    IN_PROGRESS = "IN_PROGRESS"
+    ON_HOLD = "ON_HOLD"
+    DONE = "DONE"
+
+    TASK_STATUS_CHOICES = [
+        (IN_PROGRESS, "In progress"),
+        (ON_HOLD, "ON_HOLD"),
+        (DONE, "Done"),
+    ]
+
+    collection = models.ForeignKey(
+        TaskCollection, on_delete=models.CASCADE, related_name='tasks')
+    task_id = models.UUIDField(
+        verbose_name="Task ID", primary_key=True,  unique=True, editable=False, default=uuid.uuid4)
+    task_name = models.CharField(verbose_name='Task name',
+                                 db_comment='Task name unique within a task collection', max_length=30)
+    start_date = models.DateField(verbose_name='Start date', auto_now_add=True)
+    deadline = models.DateField(verbose_name='Deadline data')
+    status = models.CharField(
+        max_length=15,
+        choices=TASK_STATUS_CHOICES,
+        default=IN_PROGRESS,
+        help_text="The current status of the task.",
+        verbose_name="Task status"
+    )
