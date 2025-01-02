46c73c38f9d249071d1ba3251869c71010f15fee
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-03 00:20:11 +0300

    configure tailwind to look for classes in frontend and template index.html files

--- pms/frontend/tailwind.config.js
+++ pms/frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,ts,tsx}"],
+  content: ["./src/**/*.{html,ts,tsx}", "./index.html", "../templates/*.html"],
   theme: {
     extend: {},
   },
