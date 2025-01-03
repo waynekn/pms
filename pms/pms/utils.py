acb021ae865ca4ed86b027f3c0d98946240ebd20
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-03 15:58:17 +0300

    create utility functions to encode a uuid to a base 62 string

--- pms/pms/utils.py
+++ pms/pms/utils.py
@@ -1,4 +1,7 @@
 import re
+import uuid
+import string
+
 
 pattern = re.compile(r'(?<!^)(?=[A-Z])')
 
@@ -20,3 +23,25 @@ def camel_case_to_snake_case(data):
         return [camel_case_to_snake_case(item) for item in data]
     else:
         return data
+
+
+BASE62_ALPHABET = string.digits + string.ascii_lowercase + string.ascii_uppercase
+
+
+def _to_base62(num):
+    """Convert an integer to a base62 string."""
+    if num == 0:
+        return BASE62_ALPHABET[0]
+
+    base62 = []
+    while num > 0:
+        base62.append(BASE62_ALPHABET[num % 62])
+        num //= 62
+
+    return ''.join(reversed(base62))
+
+
+def base_62_pk():
+    """Convert a UUID object to a base62-encoded string."""
+    uuid_obj = uuid.uuid4()
+    return _to_base62(uuid_obj.int)
