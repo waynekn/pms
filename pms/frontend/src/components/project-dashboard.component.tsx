e33d7414d2fcf9b46f3aaea0c5492630421c1274
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-03 19:13:33 +0300

    create dashboard to display project stats

---
+++ pms/frontend/src/components/project-dashboard.component.tsx
@@ -0,0 +1,150 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { BarChart } from "@mui/x-charts/BarChart";
+
+import camelize from "../utils/snakecase-to-camelcase";
+import api from "../api";
+
+type ProjectStatsResponse = {
+  tasks: number;
+  members: number;
+  description: string;
+  tasks_in_progress: number;
+  tasks_on_hold: number;
+  tasks_completed: number;
+  percentage_completion: number;
+};
+
+type ProjectStats = Omit<
+  ProjectStatsResponse,
+  | "tasks_in_progress"
+  | "tasks_on_hold"
+  | "tasks_completed"
+  | "percentage_completion"
+> & {
+  tasksInProgress: number;
+  tasksOnHold: number;
+  tasksCompleted: number;
+  percentageCompletion: number;
+};
+
+const ProjectDashBoard = () => {
+  const initialState: ProjectStats = {
+    tasks: 0,
+    members: 0,
+    description: "",
+    tasksOnHold: 0,
+    tasksInProgress: 0,
+    tasksCompleted: 0,
+    percentageCompletion: 0,
+  };
+  const [projectStats, setProjectStats] = useState<ProjectStats>(initialState);
+
+  const barColors = ["#FF5733", "#FFC300", "#28A745"];
+
+  const { projectId } = useParams();
+
+  useEffect(() => {
+    const getProjectStats = async () => {
+      try {
+        const res = await api.get<ProjectStatsResponse>(
+          `project/stats/?pk=${projectId}`
+        );
+        const stats = camelize(res.data) as ProjectStats;
+        setProjectStats(stats);
+      } catch (error) {
+        // TODO
+        console.log(error);
+      }
+    };
+    void getProjectStats();
+  }, [projectId]);
+
+  return (
+    <div className="h-full w-full flex flex-col ">
+      <article className="w-full h-1/3 md:w-3/5 border-stone-700 bg-stone-300 rounded-t-lg flex flex-col">
+        <h2 className="ml-2 flex-none">Project Description</h2>
+        <div className="flex-1 min-h-0">
+          <p className="bg-stone-100 p-2 h-full overflow-y-auto sm:text-2xl md:text-base">
+            {projectStats.description || "This project has no description"}
+          </p>
+        </div>
+      </article>
+
+      <section className="h-2/3 mt-5 border-2 rounded-lg ">
+        <div className="flex flex-col gap-5 md:flex-row md:gap-8 p-1 h-full">
+          {/* Bar Chart */}
+          <div className="w-full md:w-1/2 bg-stone-200 p-4 rounded-lg flex flex-col">
+            <h3 className="text-lg font-semibold mb-2">Bar Chart</h3>
+            <div className="rounded-md h-[300px] w-full sm:h-[400px] md:h-5/6">
+              <BarChart
+                xAxis={[
+                  {
+                    id: "barCategories",
+                    data: ["In progress", "On hold", "Completed"],
+                    scaleType: "band",
+                    label: "Task status",
+                    colorMap: {
+                      type: "ordinal",
+                      colors: barColors,
+                    },
+                  },
+                ]}
+                yAxis={[
+                  {
+                    label: "Number of tasks",
+                  },
+                ]}
+                series={[
+                  {
+                    data: [
+                      projectStats.tasksInProgress,
+                      projectStats.tasksOnHold,
+                      projectStats.tasksCompleted,
+                    ],
+                  },
+                ]}
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-5 md:flex-row md:w-1/2">
+            {/* Number of Project Members */}
+            <div className="w-full md:w-1/2 p-6 bg-gradient-to-r from-blue-500 to-blue-600 text-white border-2 border-blue-700 rounded-lg shadow-lg flex flex-col items-center">
+              <p className="text-5xl font-extrabold text-yellow-400 mb-2">
+                {projectStats.members}
+              </p>
+              <h3 className="text-xl font-semibold text-white">
+                Project Member(s)
+              </h3>
+            </div>
+
+            {/* Percentage of Tasks Completed */}
+            <div className="w-full md:w-1/2 p-6 bg-gradient-to-r from-indigo-500 to-purple-600 text-white rounded-lg shadow-lg">
+              <h3 className="text-xl font-semibold mb-3 text-gray-200">
+                Completion percentage
+              </h3>
+
+              <div className="relative">
+                <p className="text-4xl font-extrabold text-yellow-400 ml-2 text-shadow-md">
+                  {projectStats.percentageCompletion} %
+                </p>
+
+                {/* Optional Progress Bar */}
+                <div className="w-full h-2 bg-gray-200 rounded-full mt-4">
+                  <div
+                    className="h-2 rounded-full bg-yellow-400"
+                    style={{ width: `${projectStats.percentageCompletion}%` }}
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default ProjectDashBoard;
