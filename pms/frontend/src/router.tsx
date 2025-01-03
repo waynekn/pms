098e2e7d55330eb865af3c079570d95b31fbd231
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-03 19:12:19 +0300

    add routes for project detail page and project dashboard

--- pms/frontend/src/router.tsx
+++ pms/frontend/src/router.tsx
@@ -5,6 +5,8 @@ import LogInForm from "./components/login.component";
 import LogoutPage from "./pages/logout.page";
 import SignUpForm from "./components/signup.component";
 import ProfilePage from "./pages/profle.page";
+import ProjectDetailPage from "./pages/project-detail.page";
+import ProjectDashBoard from "./components/project-dashboard.component";
 import ProjectCreationPage from "./pages/project-create.page";
 import OrganizationDetail from "./pages/organization-detail.page";
 import GoogleCallback from "./components/google-callback.component";
@@ -37,6 +39,13 @@ const Router = () => {
         element={<ProjectCreationPage />}
       />
       <Route path="templates/create/" element={<TemplateCreationForm />} />
+      <Route
+        path=":projectId/:projectNameSlug/"
+        element={<ProjectDetailPage />}
+      >
+        <Route index element={<ProjectDashBoard />} />
+        <Route path="dashboard/" element={<ProjectDashBoard />} />
+      </Route>
     </Routes>
   );
 };
