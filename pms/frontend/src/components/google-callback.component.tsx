ef7a0a4a8c8515d1b77d62bb995f606f6ffd920a
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-05 16:45:09 +0300

    refactor GoogleCallback component to handle Google authentication more efficiently

--- pms/frontend/src/components/google-callback.component.tsx
+++ pms/frontend/src/components/google-callback.component.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, useLocation } from "react-router";
 
+import { selectCurrentUser } from "../store/user/user.selector";
 import { StoreDispatch } from "../store/store";
 import { googleAuth } from "../store/user/user.slice";
 
@@ -9,26 +10,29 @@ const GoogleCallback = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const dispatch = useDispatch<StoreDispatch>();
+  const currentUser = useSelector(selectCurrentUser);
+
+  const query = new URLSearchParams(location.search);
+  const code = query.get("code");
 
   history.pushState({}, "", "/");
 
   useEffect(() => {
-    const query = new URLSearchParams(location.search);
-    const code = query.get("code");
+    const sendGoogleAuthRequest = async () => {
+      if (code) {
+        try {
+          await dispatch(googleAuth(code)).unwrap();
+          await navigate(`../user/${currentUser.username}`);
+        } catch {
+          await navigate("/");
+        }
+      } else {
+        await navigate("/");
+      }
+    };
 
-    if (code) {
-      dispatch(googleAuth(code))
-        .unwrap()
-        .then((result) => {
-          console.log("Google Auth successful:", result);
-        })
-        .catch((error) => {
-          console.error("Google Auth failed:", error);
-        });
-    } else {
-      console.error("No code found in the callback URL.");
-    }
-  }, [dispatch, location, navigate]);
+    void sendGoogleAuthRequest();
+  }, [code, currentUser.username, dispatch, location, navigate]);
 
   return (
     <div className="fixed bottom-5 left-0 w-full z-10 text-center px-4 py-2 shadow-lg">
