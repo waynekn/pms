334994c2da681f87b33cb9ce1b3174d01cb788aa
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-06 12:44:13 +0300

    update username validator to allow a single white space between words

--- pms/apps/users/models.py
+++ pms/apps/users/models.py
@@ -1,11 +1,27 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from .validators import username_validator
 
 # Create your models here.
 
 
 class User(AbstractUser):
+    username = models.CharField(
+        'username',
+        max_length=150,
+        unique=True,
+        help_text=_(
+            'Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only, '
+            'with a single space allowed between words.'
+        ),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
     user_id = models.UUIDField(
         primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(
