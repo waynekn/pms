598f14a99580a973321e67f451999d28c9bcde54
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-06 12:31:00 +0300

    add username slug field

--- pms/apps/users/models.py
+++ pms/apps/users/models.py
@@ -10,6 +10,8 @@ class User(AbstractUser):
         primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(
         max_length=254, unique=True, verbose_name='email address', blank=False)
+    # make field nullable to avoid migration errors
+    username_slug = models.SlugField(max_length=200, unique=True, null=True)
 
     def __str__(self) -> str:
         return self.username
