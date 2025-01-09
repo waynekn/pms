8ac92541ccad0dfdf1a7d706d28878bdb40aecdc
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-09 18:38:40 +0300

    rename `camel_to_snake` function to `_camel_to_snake`

--- pms/pms/utils.py
+++ pms/pms/utils.py
@@ -6,7 +6,7 @@ import string
 pattern = re.compile(r'(?<!^)(?=[A-Z])')
 
 
-def camel_to_snake(name):
+def _camel_to_snake(name):
     """
     Converts a camelCase string to snake_case.
     """
@@ -18,7 +18,7 @@ def camel_case_to_snake_case(data):
     Recursively goes through the data and converts all keys from camelCase to snake_case.
     """
     if isinstance(data, dict):
-        return {camel_to_snake(key): camel_case_to_snake_case(value) for key, value in data.items()}
+        return {_camel_to_snake(key): camel_case_to_snake_case(value) for key, value in data.items()}
     elif isinstance(data, list):
         return [camel_case_to_snake_case(item) for item in data]
     else:
