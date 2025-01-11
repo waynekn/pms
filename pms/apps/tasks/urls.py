48d3372580fd047526caed89988ce6056243b4d6
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-11 22:01:56 +0300

    add url routing for task assignment

--- pms/apps/tasks/urls.py
+++ pms/apps/tasks/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('task/create/', views.TaskCreateView.as_view(), name='create_task'),
     path('task/detail/<str:task_id>/',
          views.TaskDetailView.as_view(), name='task_detail'),
+    path('task/<str:task_id>/assign/',
+         views.TaskAssignmentView.as_view(), name='assign_task'),
 ]
