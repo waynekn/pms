232094a27ff09382a3923649d9a0ef499f95ddfb
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-11 19:22:24 +0300

    add URL pattern for task detail view

--- pms/apps/tasks/urls.py
+++ pms/apps/tasks/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('task/create/', views.TaskCreateView.as_view(), name='create_task'),
+    path('task/detail/<str:task_id>/',
+         views.TaskDetailView.as_view(), name='task_detail'),
 ]
