8ae999906f413cc7bdc9bc8de12eb4a6cdc6a85e
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-11 22:10:37 +0300

    add route to enable retrieval of users who are members of a project but have not been assigned a task

--- pms/apps/tasks/urls.py
+++ pms/apps/tasks/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('task/create/', views.TaskCreateView.as_view(), name='create_task'),
     path('task/detail/<str:task_id>/',
          views.TaskDetailView.as_view(), name='task_detail'),
+    path('task/<str:task_id>/non-assignees/',
+         views.NonTaskAssigneesListView.as_view(), name='non_assignees'),
     path('task/<str:task_id>/assign/',
          views.TaskAssignmentView.as_view(), name='assign_task'),
 ]
