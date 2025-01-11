2857489dee6e81e2062ef8a7b12b9b93ca7b1752
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-11 18:26:45 +0300

    create component to display users who have been assigned a task

---
+++ pms/frontend/src/components/task-assigness.component.tsx
@@ -0,0 +1,28 @@
+import Stack from "@mui/material/Stack";
+import { ProjectMember } from "./project-members-list.component";
+
+type TaskAssignessProps = {
+  assignees: ProjectMember[];
+};
+
+const TaskAssigness = ({ assignees }: TaskAssignessProps) => {
+  return (
+    <>
+      {assignees.length > 0 ? (
+        <Stack>
+          {assignees.map((user, index) => (
+            <p
+              key={index}
+              className="font-sans font-semibold md:text-lg h-9 border-b"
+            >
+              {user.username}
+            </p>
+          ))}
+        </Stack>
+      ) : (
+        <p className="text-gray-500">No users have been assigned this task</p>
+      )}
+    </>
+  );
+};
+export default TaskAssigness;
