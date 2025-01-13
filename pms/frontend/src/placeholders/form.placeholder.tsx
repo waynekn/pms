7b5fb576a80d92c6430737350181cab351bdd0ac
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-13 16:07:31 +0300

    add placeholder component for form loading state

---
+++ pms/frontend/src/placeholders/form.placeholder.tsx
@@ -0,0 +1,33 @@
+const FormPlaceHolder = () => {
+  return (
+    <div className="border sm:w-64 md:w-72 p-3 animate-pulse">
+      <h1 className="text-xl font-bold text-center h-5 w-full bg-gray-400"></h1>
+
+      <div className="mt-4">
+        <div className="h-5 w-full bg-gray-400 "></div>
+        <div className="mt-1 w-full px-3 py-2 h-8 w-full bg-gray-400" />
+      </div>
+
+      <div className="mt-4">
+        <div className="h-5 w-full bg-gray-400" />
+        <div className="mt-1 w-full px-3 py-2 h-8 w-full bg-gray-400" />
+      </div>
+
+      <div className="mt-4">
+        <div className="h-5 w-full bg-gray-400" />
+        <div className="mt-1 w-full px-3 py-2 h-8 w-full bg-gray-400" />
+      </div>
+
+      <div className="mt-4">
+        <div className="h-5 w-full bg-gray-400" />
+        <div className="mt-1 w-full px-3 py-2 h-8 w-full bg-gray-400" />
+      </div>
+
+      <div className="mt-4">
+        <div className="h-8 w-full bg-gray-400"></div>
+      </div>
+    </div>
+  );
+};
+
+export default FormPlaceHolder;
