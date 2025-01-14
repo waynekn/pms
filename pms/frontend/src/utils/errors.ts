c73e4dc3dd2ebc9e71b4a342eb09c40cdfdd01a0
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-14 13:35:03 +0300

    add utility function to handle generic API errors with customizable messages

---
+++ pms/frontend/src/utils/errors.ts
@@ -0,0 +1,47 @@
+import { AxiosError, isAxiosError } from "axios";
+
+export type ErrorMessageConfig = {
+  400?: string;
+  404?: string;
+  500?: string;
+};
+
+const handleGenericApiErrors = (
+  error: unknown,
+  messageConfig?: ErrorMessageConfig
+): string => {
+  if (isAxiosError(error)) {
+    const statusCode = error.status;
+
+    if (statusCode === 400) {
+      const axiosError = error as AxiosError<{ detail: string }>;
+
+      return (
+        axiosError.response?.data.detail ||
+        messageConfig?.[400] ||
+        "Bad request"
+      );
+    }
+
+    if (statusCode === 404) {
+      const axiosError = error as AxiosError<{ detail: string }>;
+
+      console.log(axiosError.response?.data.detail);
+      return (
+        axiosError.response?.data.detail ||
+        messageConfig?.[404] ||
+        "Could not find the requested resource"
+      );
+    }
+
+    if (!statusCode || statusCode >= 500) {
+      return messageConfig?.[500] || "An unexpected error occurred";
+    }
+
+    return "An unexpected error occurred";
+  } else {
+    return "An unexpected error occurred";
+  }
+};
+
+export default handleGenericApiErrors;
