043084b340242e0f3da4a28fe2bdf5ca1bd07faa
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-15 17:46:48 +0300

    add task status management with Snackbar notifications

--- pms/frontend/src/pages/task-detail.page.tsx
+++ pms/frontend/src/pages/task-detail.page.tsx
@@ -3,6 +3,8 @@ import classNames from "classnames";
 import { useParams } from "react-router";
 import Container from "@mui/material/Container";
 import CircularProgress from "@mui/material/CircularProgress";
+import Alert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
 
 import { Task, TaskResponse } from "../pages/project-tasks.page";
 import TaskAssigness from "../components/task-assigness.component";
@@ -20,6 +22,11 @@ type TaskDetail = Task & {
   role: "Manager" | "Member";
 };
 
+type SnackBarState = {
+  message: string;
+  serverity: "success" | "error";
+};
+
 type Tabs = "description" | "assignees" | "add members";
 
 const TaskDetailPage = () => {
@@ -28,6 +35,7 @@ const TaskDetailPage = () => {
     taskName: "",
     startDate: "",
     deadline: "",
+    status: "IN_PROGRESS",
     role: "Member",
     description: "",
     projectPhase: { phaseId: "", phaseName: "" },
@@ -38,6 +46,14 @@ const TaskDetailPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState("");
   const { taskId } = useParams();
+
+  const initialSnackBarState: SnackBarState = {
+    message: "",
+    serverity: "success",
+  };
+  const [displaySnackBar, setDisplaySnackBar] = useState(false);
+  const [snackBarState, setSnackBarState] =
+    useState<SnackBarState>(initialSnackBarState);
 
   useEffect(() => {
     const getTaskDetail = async () => {
@@ -67,6 +83,33 @@ const TaskDetailPage = () => {
     return dateStr;
   };
 
+  const handleClose = () => {
+    setDisplaySnackBar(false);
+  };
+
+  const changeTaskStatus = async (status: TaskDetail["status"]) => {
+    setDisplaySnackBar(false);
+    setSnackBarState(initialSnackBarState);
+    try {
+      await api.put(`task/${taskDetail.taskId}/status/update/`, {
+        status,
+      });
+      setTaskDetail((detail) => ({
+        ...detail,
+        status: status,
+      }));
+      setDisplaySnackBar(true);
+      setSnackBarState({
+        message: `Task status successfully updated`,
+        serverity: "success",
+      });
+    } catch (error) {
+      const errorMsg = handleGenericApiErrors(error);
+      setSnackBarState({ message: errorMsg, serverity: "error" });
+      setDisplaySnackBar(true);
+    }
+  };
+
   if (errorMessage) {
     return (
       <p className="bg-red-600 text-white rounded-lg py-4 px-2 mt-3 md:mx-10">
@@ -91,6 +134,56 @@ const TaskDetailPage = () => {
             <h3 className="text-gray-500">
               Created on {getDate(taskDetail.startDate)}
             </h3>
+            {/* Section to change task status */}
+
+            <div className="flex justify-center mt-1">
+              <div className="flex justify-evenly w-full md:w-3/4 ">
+                <button
+                  disabled={
+                    taskDetail.status === "ON_HOLD" ||
+                    taskDetail.status === "DONE"
+                  }
+                  className={classNames(
+                    "bg-bold-yellow px-2 font-bold rounded-md",
+                    taskDetail.status === "ON_HOLD" ||
+                      taskDetail.status === "DONE"
+                      ? "cursor-not-allowed"
+                      : "cursor-pointer"
+                  )}
+                  onClick={() => changeTaskStatus("ON_HOLD")}
+                >
+                  Pause task
+                </button>
+                <button
+                  disabled={
+                    taskDetail.status === "IN_PROGRESS" ||
+                    taskDetail.status === "DONE"
+                  }
+                  className={classNames(
+                    "bg-red-orange px-2 font-bold rounded-md",
+                    taskDetail.status === "IN_PROGRESS" ||
+                      taskDetail.status === "DONE"
+                      ? "cursor-not-allowed"
+                      : "cursor-pointer"
+                  )}
+                  onClick={() => changeTaskStatus("IN_PROGRESS")}
+                >
+                  Resume
+                </button>
+                <button
+                  disabled={taskDetail.status === "DONE"}
+                  className={classNames(
+                    "bg-rich-green px-2 font-bold rounded-md",
+                    taskDetail.status === "DONE"
+                      ? "cursor-not-allowed"
+                      : "cursor-pointer"
+                  )}
+                  onClick={() => changeTaskStatus("DONE")}
+                >
+                  Complete
+                </button>
+              </div>
+            </div>
           </header>
           <div className="flex flex-col w-3/4 mx-auto bg-white shadow-lg rounded-lg p-6 relative top-10">
             <nav className="flex w-full border-b-2 border-blue-500 rounded-t-md">
@@ -149,6 +242,21 @@ const TaskDetailPage = () => {
               )}
             </section>
           </div>
+
+          <Snackbar
+            open={displaySnackBar}
+            autoHideDuration={6000}
+            onClose={handleClose}
+          >
+            <Alert
+              onClose={handleClose}
+              severity={snackBarState.serverity}
+              variant="filled"
+              sx={{ width: "100%" }}
+            >
+              {snackBarState.message}
+            </Alert>
+          </Snackbar>
         </>
       )}
     </Container>
