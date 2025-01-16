7d5c8b0ecdf8330564ca4c17257e149471976a9f
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-16 17:02:21 +0300

    add isStateRefreshed utility function to check cookie for state refresh status

--- pms/frontend/src/utils/cookies.ts
+++ pms/frontend/src/utils/cookies.ts
@@ -24,3 +24,21 @@ export const getCSRFToken = () => {
 
   return cookieValue;
 };
+
+export const isStateRefreshed = () => {
+  const NAME = "state-refreshed";
+  let cookieValue = null;
+
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, NAME.length + 1) === NAME + "=") {
+        cookieValue = decodeURIComponent(cookie.substring(NAME.length + 1));
+        break;
+      }
+    }
+  }
+
+  return cookieValue === "true";
+};
