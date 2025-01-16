1a2a109ed852a8c24b3dbffc59da2bfba09e2c4e
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-16 14:29:43 +0300

    remove unused CSRF token fetching logic

--- pms/frontend/src/utils/CSRFtoken.ts
+++ pms/frontend/src/utils/CSRFtoken.ts
@@ -1,25 +1,3 @@
-import api from "../api";
-
-/**
- * Utility function to fetch and set a CSRF token.
- *
- * This function makes an API call to retrieve the CSRF token from the server and
- * stores it in a cookie. It will only be called in the `development` environment
- * to allow the frontend to make secure POST requests to the backend.
- *
- * @throws Error if the request fails; errors are handled in the calling function.
- */
-export const fetchCSRFtoken = async () => {
-  try {
-    const response = await api.get<{ csrftoken: string }>("csrftoken/");
-    const token = response.data.csrftoken;
-    document.cookie = `$csrftoken=${token}; max-age=3600; SameSite=Lax;`;
-  } catch (error) {
-    console.error("CSRF token fetch failed:", error);
-    throw new Error("Failed to fetch CSRF token");
-  }
-};
-
 /**
  * Retrieves the CSRF token from the document's cookies.
  *
