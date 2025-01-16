58daa9497e76cde50f7805501d859cb95cb492b7
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-16 22:09:40 +0300

    add `OrganizationAdminCreateView` to assign administration privileges to organization members

--- pms/apps/organizations/views.py
+++ pms/apps/organizations/views.py
@@ -194,3 +194,45 @@ class NonOrganizationAdminsListView(generics.ListAPIView):
             organization=organization, role='Member').values_list('user', flat=True)
 
         return User.objects.filter(user_id__in=admins)
+
+
+class OrganizationAdminCreateView(generics.ListCreateAPIView):
+    """
+    Assigns members administration privilleges for an organization.
+    """
+
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        organization_id = kwargs.get('organization_id')
+        members: list[str] = request.data.get('members')
+
+        if not members:
+            return Response({'detail': 'No organization members to be added were provided'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            organization = Organization.objects.get(pk=organization_id)
+        except Organization.DoesNotExist:
+            return Response({'detail': 'No organization matches the given query'},
+                            status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            membership = OrganizationMember.objects.get(
+                organization=organization, user=self.request.user)
+        except OrganizationMember.DoesNotExist:
+            return Response({'detail': 'You dont have the necessary permissions to perform this action'},
+                            status=status.HTTP_403_FORBIDDEN)
+
+        if membership.role != 'Admin':
+            return Response({'detail': 'You dont have the necessary permissions to perform this action'},
+                            status=status.HTTP_403_FORBIDDEN)
+
+        users = User.objects.filter(username__in=members)
+
+        org_members = OrganizationMember.objects.filter(
+            organization=organization, user__in=users)
+
+        for member in org_members:
+            member.role = 'Admin'
+            member.save()
+
+        return Response(status=status.HTTP_200_OK)
