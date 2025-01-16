01e293511ecf32f6e5b256344f1968c13a3b4292
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-16 14:29:35 +0300

    refactor App component to remove unused CSRF token fetching logic

--- pms/frontend/src/App.tsx
+++ pms/frontend/src/App.tsx
@@ -1,42 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router";
-import { getCSRFToken } from "./utils/CSRFtoken";
+
 import Router from "./router";
 
-import { fetchCSRFtoken } from "./utils/CSRFtoken";
-
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    /**
-     * This effect is used to fetch a CSRF token during the development environment.
-     * It ensures that the CSRF token is retrieved before making any POST requests
-     * to the API. This call will only be made when the app is running in
-     * development mode (`import.meta.env.MODE === 'development'`).
-     */
-    const initCSRFToken = async () => {
-      try {
-        if (import.meta.env.MODE === "development") {
-          const csrfToken = getCSRFToken();
-          if (!csrfToken.trim()) {
-            await fetchCSRFtoken();
-          }
-        }
-        setIsLoading(false);
-      } catch {
-        window.location.href = "#";
-        setIsLoading(false);
-      }
-    };
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    initCSRFToken();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
   return (
     <BrowserRouter>
       <Router />
