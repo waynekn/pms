37ec1fd5ffcbc872ff844b4cc5160084e36b9692
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-16 16:59:45 +0300

    implement user state refresh on app load

--- pms/frontend/src/App.tsx
+++ pms/frontend/src/App.tsx
@@ -1,9 +1,30 @@
 import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter } from "react-router";
+
+import { StoreDispatch } from "./store/store";
+import { clearCurrentUser, fetchCurrentUser } from "./store/user/user.slice";
+import { isStateRefreshed } from "./utils/cookies";
 
 import Router from "./router";
 
 function App() {
+  const dispatch = useDispatch<StoreDispatch>();
+  useEffect(() => {
+    const refreshState = async () => {
+      if (isStateRefreshed()) {
+        return;
+      }
+      try {
+        await dispatch(fetchCurrentUser());
+      } catch {
+        dispatch(clearCurrentUser());
+      }
+      document.cookie = "state-refreshed=true";
+    };
+    void refreshState();
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Router />
