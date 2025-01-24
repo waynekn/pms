c3cc5d09d8b4aab5eda15febc3ed9139254f68c2
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-24 17:24:26 +0300

    add URL path for username updates

--- pms/apps/users/urls.py
+++ pms/apps/users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (
-    GoogleLogin, UserProfilePictureUpdateView)
+    GoogleLogin, UserProfilePictureUpdateView, UsernameUpdateView)
 
 
 urlpatterns = [
     path('avatar/update/',
          UserProfilePictureUpdateView.as_view(), name="update_avatar"),
+    path('username/update/',
+         UsernameUpdateView.as_view(), name="username_update"),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('dj-rest-auth/google/',
