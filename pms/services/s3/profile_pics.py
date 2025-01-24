2fffb534dc15d991f9b6a4959e091f3287bc9516
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-24 19:22:24 +0300

    add caching for profile picture URLs

--- pms/services/s3/profile_pics.py
+++ pms/services/s3/profile_pics.py
@@ -1,5 +1,6 @@
 import logging
 from django.conf import settings
+from django.core.cache import cache
 from django.core.files import File
 from botocore.exceptions import ClientError
 
@@ -21,9 +22,13 @@ def upload_profile_pic(picture: File, picture_name: str) -> bool:
     img_buffer = process_profile_pic(picture)
 
     picture_name = "profile-pics/" + picture_name
+    DEFAULTPROFILEPICTURE = "profile-pics/" + settings.DEFAULT_PROFILE_PICTURE
 
     try:
         s3_client.upload_fileobj(img_buffer, BUCKET_NAME, picture_name)
+
+        if picture_name != DEFAULTPROFILEPICTURE:
+            cache.delete(picture_name)
     except ClientError as e:
         logging.error(e)
         return False
@@ -40,13 +45,20 @@ def get_profile_pic_url(picture_name) -> str:
     """
     BUCKET_NAME = settings.AWS_STORAGE_BUCKET_NAME
     EXPIRATION = 3600  # 1hour
+    TIMEOUT = 59.5 * 60  # 59.5 minutes
     picture_name = "profile-pics/" + picture_name
+
+    cached_url = cache.get(picture_name)
+
+    if cached_url:
+        return cached_url
 
     try:
         url = s3_client.generate_presigned_url('get_object',
                                                Params={'Bucket': BUCKET_NAME,
                                                        'Key': picture_name},
                                                ExpiresIn=EXPIRATION)
+        cache.set(picture_name, url, timeout=TIMEOUT)
     except ClientError as e:
         logging.error(e)
         return ""
