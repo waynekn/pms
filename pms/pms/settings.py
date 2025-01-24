5e450a47e4435757a0974fe849adfd4d4c42413c
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-24 19:23:18 +0300

    add cache config

--- pms/pms/settings.py
+++ pms/pms/settings.py
@@ -133,6 +133,14 @@ DATABASES = {
     }
 }
 
+# caching
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://cache:6379",
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
