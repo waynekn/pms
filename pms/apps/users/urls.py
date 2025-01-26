1207df9d532689d3998a6adbcdd57c18f41137a4
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-26 18:36:44 +0300

    refactor: import entire views module

--- pms/apps/users/urls.py
+++ pms/apps/users/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import (
-    GoogleLogin, UserProfilePictureUpdateView, UsernameUpdateView)
+from . import views
 
 
 urlpatterns = [
     path('avatar/update/',
-         UserProfilePictureUpdateView.as_view(), name="update_avatar"),
+         views.UserProfilePictureUpdateView.as_view(), name="update_avatar"),
     path('username/update/',
-         UsernameUpdateView.as_view(), name="username_update"),
+         views.UsernameUpdateView.as_view(), name="username_update"),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('dj-rest-auth/google/',
-         GoogleLogin.as_view(), name='google_login'),
+         views.GoogleLogin.as_view(), name='google_login'),
     path('dj-rest-auth/token/refresh/',
          TokenRefreshView.as_view(), name="token_refresh"),
     path('accounts/', include('allauth.urls')),
