7c69bb454f86ef96a117038ea2c972fc2b623e42
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-27 12:49:58 +0300

    add custom registration view to set authentication tokens

--- pms/apps/users/views.py
+++ pms/apps/users/views.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from django.core.exceptions import ValidationError
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from rest_framework_simplejwt.tokens import RefreshToken
+from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.registration.views import SocialLoginView
 from rest_framework import generics, status
 from rest_framework.response import Response
@@ -18,6 +20,46 @@ from services.s3.profile_pics import upload_profile_pic, delete_profile_pic
 load_dotenv()
 
 # Create your views here.
+
+
+class AccountRegsitrationView(RegisterView):
+    """
+    Handle user registration and set authentication tokens as cookies.
+
+    This view extends the `RegisterView` from `dj-rest-auth` to customize
+    the response by setting the access and refresh tokens as HTTP-only cookies.
+    By default, `dj-rest-auth` does not set these tokens as cookies during registration.
+    """
+
+    def post(self, request, *args, **kwargs):
+
+        serializer = self.get_serializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.save(request)
+            user_response = UserDetailsSerializer(user).data
+            response = Response(user_response,
+                                status=status.HTTP_201_CREATED)
+
+            refresh = RefreshToken.for_user(user)
+            access = refresh.access_token
+
+            response.set_cookie(
+                key='access_token',
+                value=str(access),
+                httponly=True,
+                samesite='Lax',
+            )
+            response.set_cookie(
+                key='refresh_token',
+                value=str(refresh),
+                httponly=True,
+                samesite='Lax',
+            )
+
+            return response
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GoogleLogin(SocialLoginView):
