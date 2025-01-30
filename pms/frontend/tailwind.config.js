24f5b009c1de69c99387985bd2a0e0fe8a59ba7d
Wayne Kinyanjui <waynekn24@gmail.com>
2025-01-30 16:53:07 +0300

    add custom animations for fadeIn and slideIn effects

--- pms/frontend/tailwind.config.js
+++ pms/frontend/tailwind.config.js
@@ -8,6 +8,20 @@ export default {
         "bold-yellow": "#FFC300",
         "rich-green": "#28A745",
       },
+      animation: {
+        fadeIn: "fadeIn 1s ease-out",
+        slideIn: "slideIn 1s ease-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0", transform: "translateY(10px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        slideIn: {
+          "0%": { opacity: "0", transform: "translateX(-30px)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
+      },
     },
   },
   plugins: [],
