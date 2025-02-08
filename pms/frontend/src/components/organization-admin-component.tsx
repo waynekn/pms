5ee59ad0a1ee65b7363ee22be861f53bc9ece795
Wayne Kinyanjui <waynekn24@gmail.com>
2025-02-08 23:01:10 +0300

    add functionality to revoke admin privilledges

--- pms/frontend/src/components/organization-admin-component.tsx
+++ pms/frontend/src/components/organization-admin-component.tsx
@@ -2,26 +2,42 @@ import { useEffect, useState } from "react";
 import Container from "@mui/material/Container";
 import CircularProgress from "@mui/material/CircularProgress";
 import Avatar from "@mui/material/Avatar";
+import Snackbar from "@mui/material/Snackbar";
+import RemoveIcon from "@mui/icons-material/Remove";
+import Alert from "@mui/material/Alert";
 
 import {
+  OrganziationDetail,
   OrganizationMember,
   OrganizationMemberResponse,
 } from "../types/organization";
+import { SnackBarState } from "../types/snackbar";
 
 import handleGenericApiErrors, { ErrorMessageConfig } from "../utils/errors";
 import camelize from "../utils/snakecase-to-camelcase";
 import api from "../api";
+import Tooltip from "@mui/material/Tooltip";
 
 type OrganizationAdminListProps = {
   organizationId: string;
+  role: OrganziationDetail["role"];
 };
 
 const OrganizationAdminList = ({
   organizationId,
+  role,
 }: OrganizationAdminListProps) => {
   const [errorMessage, setErrorMessage] = useState<string>("");
   const [admins, setAdmins] = useState<OrganizationMember[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const initialSnackBarState: SnackBarState = {
+    message: "",
+    serverity: "success",
+  };
+  const [displaySnackBar, setDisplaySnackBar] = useState(false);
+  const [snackBarState, setSnackBarState] =
+    useState<SnackBarState>(initialSnackBarState);
 
   useEffect(() => {
     const getOrganizationAdmins = async () => {
@@ -46,6 +62,27 @@ const OrganizationAdminList = ({
     void getOrganizationAdmins();
   }, [organizationId]);
 
+  const deleteOrgAdmin = async (username: string) => {
+    try {
+      await api.put(`organizations/${organizationId}/admin/revoke/`, {
+        admin: username,
+      });
+      setAdmins(admins.filter((admin) => admin.username != username));
+      setSnackBarState({
+        message: "Admin successfully removed",
+        serverity: "success",
+      });
+      setDisplaySnackBar(true);
+    } catch (error) {
+      const errMsg = handleGenericApiErrors(error);
+      setSnackBarState({
+        message: errMsg,
+        serverity: "error",
+      });
+      setDisplaySnackBar(true);
+    }
+  };
+
   if (errorMessage) {
     return (
       <p className="bg-red-600 text-white rounded-lg py-4 px-2 mt-3 md:mx-10">
@@ -68,17 +105,44 @@ const OrganizationAdminList = ({
               className="font-sans w-full font-semibold md:text-lg border-b"
             >
               <div className="flex content-center">
-                <Avatar
-                  src={admin.profilePicture}
-                  alt="profile-picture"
-                  sx={{ width: 24, height: 24, marginRight: 1 }}
-                />
-                <span>{admin.username}</span>
+                <div className="flex content-center grow">
+                  <Avatar
+                    src={admin.profilePicture}
+                    alt="profile-picture"
+                    sx={{ width: 24, height: 24, marginRight: 1 }}
+                  />
+                  <span>{admin.username}</span>
+                </div>
+
+                {role === "Admin" && (
+                  <Tooltip title="Remove admin privelledges" placement="top">
+                    <button
+                      className="hover:bg-red-500 hover:text-white rounded-full transition-colors duration-300"
+                      onClick={() => deleteOrgAdmin(admin.username)}
+                    >
+                      <RemoveIcon />
+                    </button>
+                  </Tooltip>
+                )}
               </div>
             </li>
           ))}
         </ol>
       )}
+      <Snackbar
+        open={displaySnackBar}
+        autoHideDuration={6000}
+        onClose={() => setDisplaySnackBar(false)}
+      >
+        <Alert
+          onClose={() => setDisplaySnackBar(false)}
+          severity={snackBarState.serverity}
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          {snackBarState.message}
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };
