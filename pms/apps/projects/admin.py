7066cdefc4fbf47d115751db774e86e11ddb2838
Wayne Kinyanjui <waynekn24@gmail.com>
2025-02-13 16:39:12 +0300

    remove `CustomPhase` registration

--- pms/apps/projects/admin.py
+++ pms/apps/projects/admin.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from .models import (Industry, Template, TemplatePhase,
-                     CustomPhase, Project, ProjectMember, ProjectPhase)
+                     Project, ProjectMember, ProjectPhase)
 
 # Register your models here.
 
 admin.site.register(Industry)
 admin.site.register(Template)
 admin.site.register(TemplatePhase)
-admin.site.register(CustomPhase)
 admin.site.register(Project)
 admin.site.register(ProjectMember)
 admin.site.register(ProjectPhase)
