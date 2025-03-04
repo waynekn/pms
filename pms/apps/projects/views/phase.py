f41e0b4ad3aa0024152e332d65312e96c8bc9eb2
Wayne Kinyanjui <waynekn24@gmail.com>
2025-03-04 16:36:56 +0300

    refactor: simplify authorization check for project phase creation

--- pms/apps/projects/views/phase.py
+++ pms/apps/projects/views/phase.py
@@ -100,13 +100,9 @@ class ProjectPhaseCreateView(generics.CreateAPIView):
             return Response({'detail': 'Could not get the project.'}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            membership = models.ProjectMember.objects.get(
-                project=project, member=self.request.user)
+            models.ProjectMember.objects.get(
+                project=project, member=self.request.user, role=models.ProjectMember.MANAGER)
         except models.ProjectMember.DoesNotExist:
-            return Response({'detail': 'You are not authorized to perform this action'},
-                            status=status.HTTP_403_FORBIDDEN)
-
-        if membership.role != 'Manager':
             return Response({'detail': 'You are not authorized to perform this action'},
                             status=status.HTTP_403_FORBIDDEN)
 
